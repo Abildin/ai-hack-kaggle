196ca9c0a2c268dd566f18150d00f1b50dceb9db
Aidyn Torgayev <right-hearted@yandex.ru>
2018-05-12 16:52:02 +0300

    open dataset

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 import pandas as pd
+from pathlib import Path
+home = str(Path.home())
+pth_base =  home+"/.kaggle/competitions/ai-hack-2018-minsk-stc/"
+train_video_emb_fc_pth = pth_base + "train_video_emb_fc.csv"
+
+data_train_video_emb_fc = pd.read_csv(train_video_emb_fc_pth)
+
+print(data_train_video_emb_fc.columns)
