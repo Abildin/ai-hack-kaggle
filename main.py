c4967e578d2cf298f2bce7798b4fc9ee5685222f
Aidyn Torgayev <right-hearted@yandex.ru>
2018-05-12 17:12:18 +0300

    Primary data load and read

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import pandas as pd
 from pathlib import Path
+
+# Get home path 
 home = str(Path.home())
+
+# Define base location of datasets
 pth_base =  home+"/.kaggle/competitions/ai-hack-2018-minsk-stc/"
+
+# Dataset paths
 train_video_emb_fc_pth = pth_base + "train_video_emb_fc.csv"
+train_target_pth = pth_base + "train_target.csv"
 
-data_train_video_emb_fc = pd.read_csv(train_video_emb_fc_pth)
+# Load data
+train_in = pd.read_csv(train_video_emb_fc_pth)
+train_out = pd.read_csv(train_target_pth)
 
-print(data_train_video_emb_fc.columns)
+# Choose all points for learning (ommitting 'ID' column)
+cols = [str(x) for x in range(1024)]
+X = train_in[cols]
+print(X)
